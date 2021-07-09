d162ef56607d4f739bef8d15108e7c42bd25858a
PabloG <pgaldo@formacion.cic.es>
2021-07-09 13:30:17 +0200

    Primeras dos clases creadas

---
+++ src/main/java/es/cic/bootcamp/ejercicio002/Coche.java
@@ -0,0 +1,12 @@
+package es.cic.bootcamp.ejercicio002;
+
+public class Coche {
+	
+	Motor motor = new Motor();
+	double potenciamotor = motor.saberPotencia();
+	
+	
+	
+	
+
+}
---
+++ src/main/java/es/cic/bootcamp/ejercicio002/Motor.java
@@ -0,0 +1,12 @@
+package es.cic.bootcamp.ejercicio002;
+
+
+
+public class Motor {
+	
+	public double saberPotencia() {
+ double potencia =  Math.random() * (40 - 20) / 7;
+return potencia;
+	}
+
+}
